b8dc151536fe42b2c0cf33be8c7a3155b861cbdd
Evin1- <edwin.hdz.12.0@gmail.com>
2016-08-17 12:42:44 -0400

    Add doMagic button

--- app/src/main/java/com/example/activityresult/MainActivity.java
+++ app/src/main/java/com/example/activityresult/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.activityresult;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_CONTACT_REQUEST = 10;
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doMagic(View view) {
+
     }
 }
